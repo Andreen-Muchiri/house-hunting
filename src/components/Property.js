009c130d93f1c355c95652ef7f26be85386ec03b
Andreen-Muchiri <andreenshii@gmail.com>
2022-10-06 01:32:51 +0300

    Add property component

--- src/components/Property.js
+++ src/components/Property.js
@@ -1,7 +1,21 @@
+import React from "react"
+import Heading from "./header/Heading"
+import "./property.css"
+import PropertyCard from "./PropertyCard"
 
-function Property(){
-    return(
-        <h1>Property</h1>
-    )
+function Property()  {
+  return (
+    <>
+      <section className='property background'>
+        <div className='container'>
+          <Heading title='Featured Property Types' subtitle='Find Any kind of property your looking for.' />
+    
+       <PropertyCard />
+        </div>
+      </section>
+    </>
+  )
 }
+
 export default Property;
+
---
+++ src/components/PropertyCard.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { property } from "./data/Data"
+
+function  PropertyCard () {
+  return (
+    <>
+      <div className='content grid5 mtop'>
+        {property.map((items, index) => (
+          <div className='box' key={index}>
+            <img src={items.cover} alt='' />
+            <h4>{items.name}</h4>
+            <label>{items.total}</label>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default PropertyCard;
