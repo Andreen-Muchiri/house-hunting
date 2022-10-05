3697733e8241b04a1a8b2bfe31751d9c0ec54049
Andreen-Muchiri <andreenshii@gmail.com>
2022-10-06 01:02:07 +0300

    Add contact component

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,30 @@
+import React from "react"
+import img from "./img/about.jpg"
+import "./about.css"
+import  Back from "./header/Back";
+import Heading from "./header/Heading";
 
 function About(){
     return(
-        <h1>About</h1>
+        <>
+        <section className='about'>
+          <Back name='About Us' title='About Us - Who We Are?' cover={img} />
+          <div className='container flex mtop'>
+            <div className='left row'>
+              <Heading title='Our Agency Story' subtitle='Check out our company story ' />
+  
+              <p>We all know the struggles that we go through during house hunting, particularly on the off chance that we really can't get a home inside our set models as far as location, budget, and features are concerned.</p>
+              <p>Rental_Homez is a site that is very much intended to furnish clients with a more straightforward house hunting experience. The client will be in a position to get sufficient details of different houses available to be purchased or leased in view of their requirements and models.
+  
+  </p>
+              <button className='btn2'>More About Us</button>
+            </div>
+            <div className='right row'>
+              <img src='./img/immio.jpg' alt='' />
+            </div>
+          </div>
+        </section>
+      </>
     )
 }
 export default About;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,30 @@
+import React from "react"
+import img from "./img/pricing.jpg"
+import Back from "./header/Back"
+import "./contact.css"
 
-function Contact(){
-    return(
-        <h1>Contact</h1>
-    )
+function Contact() {
+  return (
+    <>
+      <section className='contact mb'>
+        <Back name='Contact Us' title='Reach Us To Get Helps & Friendly Support' cover={img} />
+        <div className='container'>
+          <form className='shadow'>
+            <h4>Fillup The Form</h4> <br />
+            <div>
+              <input type='text' placeholder='Name' />
+              <input type='text' placeholder='Email' />
+             <input type='text' placeholder='Subject' />
+            </div>
+            <textarea cols='30' rows='10'></textarea>
+            <button>Submit Request</button>
+          </form>
+        </div>
+      </section>
+    </>
+  )
 }
-export default Contact;
+
+export default Contact
+
+
